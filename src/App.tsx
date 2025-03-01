052c11a82924a783951a5b53195e4962300c123e
Robin Blondin <robin.blondin@protonmail.com>
2025-03-01 11:01:46 +0100

    Set up header and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import  HeaderSidebar  from './components/HeaderSidebar/HeaderSidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#6b9080"
+      },
+      secondary: {
+        main: "#a4c3b2"
+      },
+      background: {
+        default: "#eaf4f4"
+      },
+      text: {
+        primary: "#6b9080",
+        secondary: "#a4c3b2"
+      }
+    }
+  });
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <HeaderSidebar />
+      <Container />
+    </ThemeProvider>
   )
 }
 
