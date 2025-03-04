7ac61703e392a1b6af3c8bb010d3af3c262c54b2
Robin Blondin <robin.blondin@protonmail.com>
2025-03-04 20:54:23 +0100

    Created slide admin ciomponent

---
+++ src/components/SlideAdmin/SlideAdmin.tsx
@@ -0,0 +1,15 @@
+import { Container } from '@mui/material'
+import React from 'react'
+import './SlideAdmin.css'
+
+const SlideAdmin: React.FC = () => {
+  return (
+    <>
+      <Container className="main-container">
+
+      </Container>
+    </>
+  )
+}
+
+export default SlideAdmin
