c8eb8d6a53856dc527465e5456325c14cff0c955
Robin Blondin <robin.blondin@protonmail.com>
2025-03-08 13:11:22 +0100

    Added fields to model

--- src/models/SlideModel.ts
+++ src/models/SlideModel.ts
@@ -1,9 +1,13 @@
 export class SlideModel {
     id: string | null;
     url: string;
+    created: string;
+    createdBy: string;
 
-    constructor(id: string | null, url: string) {
+    constructor(id: string | null, url: string, created: string, createdBy: string) {
         this.id = id;
-        this.url = url
+        this.url = url;
+        this.created = created;
+        this.createdBy = createdBy
     }
 }
