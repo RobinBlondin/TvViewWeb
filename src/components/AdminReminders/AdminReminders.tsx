dda5b71437cb9ee9eeee985a7088465e681365f4
Robin Blondin <robin.blondin@protonmail.com>
2025-03-13 20:26:49 +0100

    Designed layout of admin vew of reminders

--- src/components/AdminReminders/AdminReminders.tsx
+++ src/components/AdminReminders/AdminReminders.tsx
@@ -1,121 +1,185 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Button, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, IconButton } from '@mui/material';
-import { ReminderModel } from '../../models/ReminderModel'; // Adjust the import path as needed
-import ReminderModal from './ReminderModal'; // Import the modal component
-import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { Box, Button, Container, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import dayjs, { Dayjs } from 'dayjs';
+import React, { useEffect, useState } from 'react';
+import { ReminderModel } from '../../models/ReminderModel';
+import { createReminder, deleteReminderById, getAllReminders } from '../../service/reminderService';
 
 const AdminReminders: React.FC = () => {
   const [reminders, setReminders] = useState<ReminderModel[]>([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedReminder, setSelectedReminder] = useState<ReminderModel | null>(null);
-
-  const handleAddReminder = () => {
-    setSelectedReminder(null);
-    setIsModalOpen(true);
-  };
+  const [description, setDescription] = useState<string | null>(null)
+  const [expiryDate, setExpiryDate] = useState<Dayjs | null | undefined>(dayjs());
+  const [nextExpiryDate, setNextExpiryDate] = useState<Date>()
 
   useEffect(() => {
-    const reminder = new ReminderModel('1', 'Test', 'Test reminder', new Date(), new Date());
-    setReminders([reminder]);
+    const fetchData = async () => {
+      const events = await getAllReminders();
+      console.log(updatedReminders);
+      setReminders(events);
+    };
+    fetchData();
+
+    const updatedReminders = deleteExpiredReminders(reminders)
+    const nextExpiryDate = updatedReminders.length > 0 ? updatedReminders[0].expiryDate : null;
+      setNextExpiryDate(new Date(nextExpiryDate!));
+      
+
+
   }, []);
 
-  
-  const handleEditReminder = (reminder: ReminderModel) => {
-    setSelectedReminder(reminder);
-    setIsModalOpen(true);
-  };
+  useEffect(() => {
+    const millisecondsToNextExpiry = nextExpiryDate ? dayjs().diff(nextExpiryDate) : 0;
+    const intervalId = setTimeout(() => {
+      const updatedReminders = deleteExpiredReminders(reminders);
 
-  const handleSubmitReminder = (data: { title: string; description: string; startDate: string; expiryDate: string }) => {
-    if (selectedReminder) {
-      const updatedReminders = reminders.map((reminder) =>
-        reminder.id === selectedReminder.id
-          ? new ReminderModel(reminder.id, data.title, data.description, new Date(data.startDate), new Date(data.expiryDate))
-          : reminder
-      );
+      const nextExpiryDate = updatedReminders.length > 0 ? updatedReminders[0].expiryDate : null;
+      if(nextExpiryDate) {;
+        setNextExpiryDate(new Date(nextExpiryDate!))
+      };
       setReminders(updatedReminders);
-    } else {
-      const newReminder = new ReminderModel(
-        Date.now().toString(),
-        data.title,
-        data.description,
-        new Date(data.startDate),
-        new Date(data.expiryDate)
-      );
-      setReminders([...reminders, newReminder]);
+
+    }, millisecondsToNextExpiry);
+
+    return () => clearInterval(intervalId);
+  }, [nextExpiryDate]);
+
+  const handleDescriptionChange = (info: any) => {
+    setDescription(info.target.value)
+  }
+
+  const handleEndChange = (info: any) => {
+    setExpiryDate(info.target.value)
+  }
+
+  const handleSubmitReminder = () => {
+    if (!description || !expiryDate) {
+      return;
     }
-    setIsModalOpen(false);
+
+    const reminder = new ReminderModel(null, description, expiryDate.toISOString());
+    createReminder(reminder).then((reminder) => {
+      setReminders([...reminders, reminder])
+    });
+
+    setDescription(null);
+    setExpiryDate(dayjs());
   };
 
   const handleDeleteReminder = (id: string) => {
+    deleteReminderById(id);
+
     const updatedReminders = reminders.filter((reminder) => reminder.id !== id);
     setReminders(updatedReminders);
   };
 
+
+
+  const formatDate = (dateInput: Date) => {
+    const dateStr = dateInput.toString();
+
+    const [date, time] = dateStr.split("T");
+    const [hour, minute] = time.split(":")
+    return `${date} ${hour}:${minute}`;
+  };
+
+  const deleteExpiredReminders = (reminders: ReminderModel[]) => {
+    const currentDate = dayjs().toDate();
+
+    if(reminders.length <= 0) {
+      return reminders;
+    }
+
+    const expiredReminders = reminders.filter((reminder) => {
+      return new Date(reminder.expiryDate) < currentDate;
+    });
+
+    const updatedReminders = reminders.filter((reminder) => {
+      return new Date(reminder.expiryDate) > currentDate;
+    });
+
+    console.log("Expired reminders: ", expiredReminders);
+    console.log("Updated reminders: ", updatedReminders);
+
+    expiredReminders.forEach((reminder) => {
+      deleteReminderById(reminder.id!);
+    });
+
+    return updatedReminders;
+  }
+
   return (
-    <Box sx={{ padding: 3 }}>
-      <Typography variant="h4" gutterBottom>
-        Manage Reminders
-      </Typography>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <Box sx={{ 
+      display: "flex", 
+      justifyContent: "start", 
+      alignItems: "start", 
+      height: "80%", 
+      width: "75%", 
+      gap: 3 
+   }}>
+      
+        <Container sx={{ width: "30%", display: "flex", flexDirection: "column", justifyContent: "start", margin: 0, gap: 2, padding: 3, backgroundColor: "background.paper", borderRadius: "20px 0 20px 20px", border: "2px solid" }}>
+          <TextField 
+            label="Description" 
+            variant="outlined" 
+            onChange={(info) => handleDescriptionChange(info)}
+            value={description? description : ""}
+          />
+          <DateTimePicker  
+            value={expiryDate}
+            onChange={(info) => handleEndChange(info)}
+            label="Expiry Date"
+          />
 
-      {/* Button to open the modal for adding a new reminder */}
-      <Button variant="contained" color="primary" onClick={handleAddReminder} sx={{ marginBottom: 4 }}>
-        Add New Reminder
-      </Button>
-
-      {/* Modal for adding/editing reminders */}
-      <ReminderModal
-        open={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onSubmit={handleSubmitReminder}
-        initialData={
-          selectedReminder
-            ? {
-                title: selectedReminder.title,
-                description: selectedReminder.description,
-                startDate: selectedReminder.startDate.toISOString().slice(0, 16),
-                expiryDate: selectedReminder.expiryDate.toISOString().slice(0, 16),
-              }
-            : undefined
-        }
-      />
-
-      {/* Table to display existing reminders */}
-      <Typography variant="h6" gutterBottom>
-        Reminders List
-      </Typography>
-      <TableContainer className="table-container" component={Paper} sx={{ minWidth: '100%' }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Title</TableCell>
-              <TableCell>Description</TableCell>
-              <TableCell>Start Date</TableCell>
-              <TableCell>Expiry Date</TableCell>
-              <TableCell>Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {reminders.map((reminder) => (
-              <TableRow key={reminder.id}>
-                <TableCell>{reminder.title}</TableCell>
-                <TableCell>{reminder.description}</TableCell>
-                <TableCell>{reminder.startDate.toLocaleString()}</TableCell>
-                <TableCell>{reminder.expiryDate.toLocaleString()}</TableCell>
-                <TableCell>
-                  <IconButton onClick={() => handleEditReminder(reminder)}>
-                    <EditIcon />
-                  </IconButton>
-                  <IconButton onClick={() => handleDeleteReminder(reminder.id!)}>
-                    <DeleteIcon />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+          <Button variant="contained" color="primary" onClick={handleSubmitReminder} sx={{ marginBottom: 4, borderRadius: "0 10px 0 10px" }}>
+              <AddIcon />
+              Add Reminder
+            </Button>
+        </Container>
+      
+      <Box 
+        className="reminders-container"
+        sx={{ 
+          padding: 5, 
+          border: "2px solid", 
+          backgroundColor: "background.paper", 
+          borderRadius: "0 20px 20px 20px",
+          minHeight: "75%",
+          width: "70%",
+        }}>
+        
+          <TableContainer className="table-container" sx={{ border: "1px solid", borderRadius: "5px"}}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow sx={{width: "100%"}}>
+                <TableCell sx={{width: "60%", fontWeight: "bold"}}>Description</TableCell>
+                  <TableCell sx={{width: "30%", fontWeight: "bold"}}>Expiry Date</TableCell>
+                  <TableCell sx={{width: "10%", fontWeight: "bold"}}>Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {reminders.map((reminder) => (
+                  <TableRow key={reminder.id}>
+                    <TableCell>{reminder.description}</TableCell>
+                    <TableCell>{reminder.expiryDate}</TableCell>
+                    <TableCell>
+                      <IconButton onClick={() => handleDeleteReminder(reminder.id!)}>
+                        <DeleteIcon color="error"/>
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        
+      </Box>
     </Box>
+    </LocalizationProvider>
   );
 };
 
