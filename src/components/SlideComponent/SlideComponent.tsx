a8b0806682d32107f1cdd23fd9188df01d285a17
Robin Blondin <robin.blondin@protonmail.com>
2025-04-13 13:37:43 +0200

    Adjusted styles for  slide component

--- src/components/SlideComponent/SlideComponent.tsx
+++ src/components/SlideComponent/SlideComponent.tsx
@@ -1,57 +1,48 @@
-import { Container } from '@mui/material';
+import { Box } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { SlideModel } from '../../models/SlideModel';
 import { getAllSlides } from '../../service/slideService';
 import useWebSocket from 'react-use-websocket';
+import './SlideComponent.css';
 
 const SlideComponent: React.FC = () => {
   const [slides, setSlides] = useState<SlideModel[]>([]);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const { lastMessage } = useWebSocket("ws://localhost:8080/ws")
+  const { lastMessage } = useWebSocket("ws://localhost:8080/ws");
 
-  
   useEffect(() => {
-    console.log("fetching slides");
     const fetchSlides = async () => {
       await getAllSlides().then(response => {
         setSlides(response);
       });
     };
-
     fetchSlides();
   }, [lastMessage]);
 
   useEffect(() => {
     let intervalId: any;
-
     if (slides.length > 0) {
       intervalId = setInterval(() => {
         setCurrentIndex(prevIndex => (prevIndex + 1) % slides.length);
-      }, 3000);
+      }, 10000);
     }
-
     return () => {
-      if (intervalId) {
-        clearInterval(intervalId);
-      }
+      if (intervalId) clearInterval(intervalId);
     };
   }, [slides]);
 
-  
   return (
-    <Container
-      className="main-container"
+    <Box
       sx={{
-        width: "100%",
-        height: "100%",
-        borderRadius: "1em",
+        width: '100%',
+        height: '100%',
         backgroundImage: `url(${slides[currentIndex] ? slides[currentIndex].url : ""})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat'
+        backgroundRepeat: 'no-repeat',
+        borderRadius: "0.5em",
       }}
-    >
-    </Container>
+    />
   );
 };
 
