e304c603e7c38aec610f0b922c132ec3fcac1b88
Robin Blondin <robin.blondin@protonmail.com>
2025-04-14 22:56:24 +0200

    Set up model and service for fetching highway bridge openings

---
+++ src/service/highwayBridgeService.ts
@@ -0,0 +1,8 @@
+import { apiClient } from "./apiClient";
+import { HighwayBridgeModel } from "../models/HighwayBridgeModel"
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL + "/api/highway-bridge-opening";
+
+export const getHighwayBridgeOpenings = async (): Promise<HighwayBridgeModel[]> => {
+    return apiClient.get(`${API_BASE_URL}/all`).then((res) => res.data)
+}
