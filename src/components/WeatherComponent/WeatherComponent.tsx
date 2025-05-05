b64c6096bf482c1b65772c27de93b7f60d399b87
Robin Blondin <robin.blondin@protonmail.com>
2025-05-06 01:03:51 +0200

    Added function to set the icon based on weather and sunset times.

--- src/components/WeatherComponent/WeatherComponent.tsx
+++ src/components/WeatherComponent/WeatherComponent.tsx
@@ -2,13 +2,38 @@ import React, { useEffect, useState } from 'react'
 import { getWeatherData } from '../../service/weatherService';
 import { WeatherModel } from '../../models/WeatherModel';
 import { Container } from '@mui/material';
+import { WeatherMap } from './WeatherMap';
+import './WeatherComponent.css'
 
 
 const WeatherComponent: React.FC = () => {
     const [weatherData, setWeatherData] = useState<WeatherModel>()
+    const [weatherIcon, setWeatherIcon] = useState<string>()
+    const [weatherDesc, setWeatherDesc] = useState<string>()
 
     const fetchWeaterData = async () => {
         await getWeatherData().then(data => setWeatherData(data));
+    }
+
+    const updateWeatherIcon = (weatherCode: number | undefined) => {
+        if(!isSunSet()) {
+            setWeatherIcon(WeatherMap[weatherCode || "0"]?.day?.image)
+            setWeatherDesc(WeatherMap[weatherCode || "0"]?.day?.description)
+        } else {
+            setWeatherIcon(WeatherMap[weatherCode || "0"]?.night?.image)
+            setWeatherDesc(WeatherMap[weatherCode || "0"]?.night?.description)
+        }
+    }
+
+    const displayWindSpeedAsMeterPerSecond = (speed: number | undefined): number => {
+        return Math.round((speed || 0 / 3.6) * 10) / 10
+    }
+
+    const isSunSet = (): boolean => {
+        const now = new Date();
+        const sunset = new Date(weatherData?.sunset || "");
+        const sunrise = new Date(weatherData?.sunrise || "");
+        return now > sunset && now < sunrise
     }
 
     useEffect(() => {
@@ -17,6 +42,10 @@ const WeatherComponent: React.FC = () => {
             fetchWeaterData();
         }, 1000 * 60 );
     }, [])
+
+    useEffect(() => {
+        updateWeatherIcon(weatherData?.weatherCode)
+    }, [weatherData])
 
     
    
@@ -30,9 +59,14 @@ const WeatherComponent: React.FC = () => {
                 fontSize: "3em"
             }}
         >
-            <div>Temp: {weatherData?.temperature }</div>
-            <div>Wind speed: { weatherData?.windSpeed }</div>
+            <img src={weatherIcon} alt="" className="weather-icon"/>
+            
+            <div>{weatherData?.temperature }°C</div>
+            <div>Wind speed: { displayWindSpeedAsMeterPerSecond( weatherData?.windSpeed) }m/s</div>
             <div>Weather code: { weatherData?.weatherCode }</div>
+            <div>Weather desc: { weatherDesc }</div>
+
+            
 
         </Container>
     )
