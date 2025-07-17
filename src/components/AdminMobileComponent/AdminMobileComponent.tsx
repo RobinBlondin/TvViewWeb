b9e748ac58864b23bf9eb10e19b1a3cc499f43aa
Robin Blondin <robin.blondin@protonmail.com>
2025-07-17 20:06:20 +0200

    started design of admin mobile component

--- src/components/AdminMobileComponent/AdminMobileComponent.tsx
+++ src/components/AdminMobileComponent/AdminMobileComponent.tsx
@@ -1,19 +1,63 @@
+import { AddIcCallOutlined, Add, Edit } from "@mui/icons-material";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import React from "react";
+import "./AdminMobileComponent.css";
 
 const AdminMobileComponent: React.FC = () => {
+  const handleClick = (action: string) => {
+    console.log(action);
+  };
+
   return (
     <Box
       sx={{
-        width: "100% !important",
-        height: "100% !important",
+        width: "100vw !important",
+        height: "100vh !important",
+        padding: "0 !important",
+        margin: "0 !important",
         display: "flex",
+        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        background: "white",
+        gap: "1em",
+        background: "radial-gradient(circle at top, #6e3a40, #4c2b2f, #2e1a1e)",
       }}
     >
-      <div>Admin mobile view</div>
+      <Button
+        className="menu-button"
+        variant="contained"
+        sx={{ textAlign: "start" }}
+        startIcon={<Add />}
+        onClick={() => handleClick("add-reminder")}
+      >
+        Quick Add Slide
+      </Button>
+      <Button
+        className="menu-button"
+        variant="contained"
+        startIcon={<Add />}
+        onClick={() => handleClick("add-slides")}
+      >
+        QUICK ADD REMINDER
+      </Button>
+
+      <Button
+        className="menu-button"
+        variant="contained"
+        startIcon={<Edit />}
+        onClick={() => handleClick("edit-reminders")}
+      >
+        Add New Slide
+      </Button>
+      <Button
+        className="menu-button"
+        variant="contained"
+        startIcon={<Edit />}
+        onClick={() => handleClick("edit-slides")}
+      >
+        Add New Slide
+      </Button>
     </Box>
   );
 };
