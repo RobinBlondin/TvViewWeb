33decae44da42bf745a9016c7d87535c397d51a7
Robin Blondin <robin.blondin@protonmail.com>
2025-07-17 19:23:13 +0200

    Create admin view for mobile devices

---
+++ src/components/AdminMobileComponent/AdminMobileComponent.tsx
@@ -0,0 +1,21 @@
+import Box from "@mui/material/Box";
+import React from "react";
+
+const AdminMobileComponent: React.FC = () => {
+  return (
+    <Box
+      sx={{
+        width: "100% !important",
+        height: "100% !important",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "white",
+      }}
+    >
+      <div>Admin mobile view</div>
+    </Box>
+  );
+};
+
+export default AdminMobileComponent;
