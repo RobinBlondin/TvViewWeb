07ffb09d78b87a0946cd39ce20a122be03a29ced
Robin Blondin <robin.blondin@protonmail.com>
2025-07-19 15:34:08 +0200

    Added util function for formatting time.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,3 +8,16 @@ export const getWindowWidth = (): number => {
     document.body.clientWidth
   );
 };
+
+export const formatDateToLocaleString = (date: string): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
+  return new Date(date).toLocaleString(undefined, options);
+};
